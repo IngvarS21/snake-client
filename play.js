af50b8a4e3a3a8ee83951c62999ec12cf8ffe5f9
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-27 21:55:19 +0000

    Updated snake client

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function (connection) {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: "50541"
-  });
-  
-  conn.on("data", (data) => {
-    console.log(`you ded cuz you idled`);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const {connect} = require ("./client");
 
 console.log("Connecting ...");
 connect();
