9a57ce1194c98f6351835d4f58f30210ddbe15b4
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-10-29 19:56:45 +0000

    added connection to the server and movement buttons

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection; 
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -8,9 +11,29 @@ const setupInput = function () {
 };
 
 const handleUserInput = function (key) {
-    if (key === '\u0003') {
+  
+  if (key === '\u0003') {
       process.exit();
     }
-};
+    if (key === 'w') {
+      console.log("Up is pressed.");
+      connection.write("Move: up");
+    }
+    if (key === 's') {
+      console.log("Down is pressed.");
+      connection.write("Move: down");
+    }
+    if (key === 'a') {
+      console.log("Left is pressed.")
+      connection.write("Move: left");
+    }
+    if (key === 'd') {
+      console.log("Right is pressed.")
+      connection.write("Move: right");
+    }
+  };
+  
+
+
 
 module.exports = {setupInput};
